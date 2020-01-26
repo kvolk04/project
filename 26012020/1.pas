600516b76c90d2c7b00836aac29167342845e4d8
Qvocha <pineappletoe@gmail.com>
2020-01-26 19:24:39 +0300

    Numbers-2_10 done

---
+++ 26012020/1.pas
@@ -0,0 +1,17 @@
+﻿Program Numbers_m_10;
+var N10, m, Nm, k: longint;
+begin
+  write('m=');
+  readln(m);
+  write('Nm=');
+  readln(Nm);
+  k := 1;
+  N10 := 0;
+  while (Nm <> 0) do
+  begin
+    N10 := N10 + (Nm mod 10) * k;
+    k := k * m;
+    Nm := Nm div 10;
+  end;
+  writeln('N10=', N10)
+end.
