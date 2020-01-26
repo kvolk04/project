69f6b1eda8d1b8ae790fcd685eae8e961bbdc3cf
Qvocha <pineappletoe@gmail.com>
2020-01-26 19:30:05 +0300

    Create 2.pas

---
+++ 26012020/2.pas
@@ -0,0 +1,16 @@
+﻿Program Numbers_10_m;
+var N10, Nm, k, m: longint;
+begin
+  write('N10=');
+  readln(N10);
+  write('m=');
+  readln(m);
+  k := 1;
+  Nm := 0;
+  repeat
+    Nm := Nm + (N10 mod m) * k;
+    k := k * 10;
+    N10 := N10 div m;
+  until (N10 = 0);
+  writeln('Nm=', Nm)
+end.
