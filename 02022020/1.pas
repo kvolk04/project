dd5c0873feaf9cffc4fec0a69b0ad21c3181bbb0
Qvocha <pineappletoe@gmail.com>
2020-02-02 18:32:16 +0300

    hw done

---
+++ 02022020/1.pas
@@ -0,0 +1,25 @@
+﻿uses
+  crt;
+
+var
+  x0, y0: integer;
+
+begin
+  window(1, 1, 80, 25);
+  textbackground(7);
+  clrscr;
+  
+  textcolor(12);
+  
+  x0 := random(1, 80);
+  y0 := random(1, 25);
+  gotoxy (x0, y0);
+  write ('*');
+  for var x := x0 to 80 - x0 do
+  begin
+    delay(100);
+    gotoxy(x, y0);
+    write('*');
+  end;
+  writeln;
+end.
---
+++ 02022020/2.pas
@@ -0,0 +1,28 @@
+﻿uses
+  crt;
+
+var
+  x, y, color, number: integer;
+
+begin
+  window(1, 1, 80, 25);
+  textbackground(7);
+  clrscr;
+  
+  for var i := 1 to 5 do
+  begin
+    x := random(1, 80);
+    y := random(1, 25);
+    gotoxy (x, y);
+    number := random(10, 99);
+    color := random(0, 15);
+    textcolor(color);
+    delay(2000);
+    write(number);
+    delay(2000);
+    textcolor(7);
+    gotoxy (x, y);
+    write(number);
+  end;
+  writeln;
+end.
